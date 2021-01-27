aa43912bf2bba613ac6f3a3b009d36cf34c2a640
madcbal <69820577+madcbal@users.noreply.github.com>
2021-01-27 16:23:51 -0500

    Back To Top Button
    added a back to top button in the html, css, and js files

--- Unit_18_HW_Template/js/index.js
+++ Unit_18_HW_Template/js/index.js
@@ -1,5 +1,19 @@
 console.log("Your index.js file is loaded correctly!");
 
-$("p").click(function(){
-    $(this).scroll();
-});
+mybutton = document.getElementById("myBtn");
+
+window.onscroll = function () {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 ||
+document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
